172f13c749f90d15466cf954fccc2673560ece2f
tms-5 <tms5@cin.ufpe.br>
2024-05-06 21:06:10 -0300

    chore: Update Main component layout and add buttons for learning and getting started

--- src/lib/components/appComponents/Main/Main.svelte
+++ src/lib/components/appComponents/Main/Main.svelte
@@ -12,8 +12,11 @@
 
 <section class="d-grid justify-items-center pb-2r">
   <div class="w-80">
-    <div class="fw-800 f-2 c-light-blue-100 text-center align-center d-flex justify-center">
-      <img src={logo} height="40" alt="Logo" /> <span class="c-light-blue-200">GO</span>Teach
+    <div
+      class="fw-800 f-2 c-light-blue-100 text-center align-center d-flex justify-center"
+    >
+      <img src={logo} height="40" alt="Logo" />
+      <span class="c-light-blue-200">GO</span>Teach
     </div>
     <p class="c-light-purple-50">
       The developed tool aims to facilitate the implementation of evidence-based
@@ -28,12 +31,20 @@
       classes.
     </p>
   </div>
-  <Button
-    label="Get Started"
-    onClick={() => goto("/prompt-generator")}
-    className="w-auto"
-    style="septenary"
-  />
+  <div class="d-flex gap-1r">
+    <Button
+      label="Learn More About The Strategies"
+      onClick={() => goto("/prompt-generator")}
+      className="w-auto"
+      style="default"
+    />
+    <Button
+      label="Get Started With The Tool"
+      onClick={() => goto("/prompt-generator")}
+      className="w-auto"
+      style="septenary"
+    />
+  </div>
 </section>
 <!-- <section class="bg-light-blue-50 p-2">
   <div class="d-flex">
